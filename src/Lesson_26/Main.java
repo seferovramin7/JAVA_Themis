43f0cba7aca99d1b73f423eb3b4cf496aa142a03
seferovramin7 <seferovramin7@gmail.com>
2025-01-25 11:59:20 +0400

    Liste 5 elave etdim

--- src/Lesson_26/Main.java
+++ src/Lesson_26/Main.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List.of(1, 2, 3, 4).parallelStream()
+        List.of(1, 2, 3, 4, 5).parallelStream()
                 .forEach(System.out::println);
 
     }
